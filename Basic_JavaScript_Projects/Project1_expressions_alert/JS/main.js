7483311aff7d26f90e8fcf607ee854f9398d9de2
JRHowenstine <justin.howenstine@gmail.com>
2022-11-12 13:26:10 -0800

    Basic Javascript Projects Submission Assignment

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,11 @@
+
+window.alert("Hello, World!"); //Alert when page is first opened
+var Family = "The Howenstines", Mom = "Madeleine", Oldest = "Josiah", Middle = "Jared", Youngest = "Justin"; //Assigned variables
+document.write(Youngest); //Print value of variable Youngest
+document.write(4*5); //Print the value of the expression
+function My_First_Function() { //defining the function and naming it
+    var str="This is the button text!";// defining a variable and giving it a string value
+    document.getElementById("Button_Text").innerHTML=str;//Putting the value of the variable into HTML elementFromPoint with the "Button_Text" id
+}
+var Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string"; //Set values of variables Sent1 and Sent2
+document.write(Sent1 + Sent2); //concatenate variables Sent1 and Sent2 to make the full sentence
