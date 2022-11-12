a37bdc443de911cbc3292c052112836bcf773037
JRHowenstine <justin.howenstine@gmail.com>
2022-11-12 13:41:30 -0800

    Added onmouseover event

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -9,3 +9,13 @@ function My_First_Function() { //defining the function and naming it
 }
 var Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string"; //Set values of variables Sent1 and Sent2
 document.write(Sent1 + Sent2); //concatenate variables Sent1 and Sent2 to make the full sentence
+
+function bigImg(x) { //Created function to increase image size
+    x.style.height = "150px";
+    x.style.width = "200px";
+  }
+  
+function normalImg(x) { //Created functionfor normal image size
+    x.style.height = "75px";
+    x.style.width = "100px";
+  }
