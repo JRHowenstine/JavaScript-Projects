95270c6e207346e69fc6fa6ba54f758446a0b25d
JRHowenstine <justin.howenstine@gmail.com>
2022-11-13 10:38:44 -0800

    Project4_dictionaries

---
+++ Basic_JavaScript_Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,22 @@
+function my_Dictionary1() { //Define and Name function
+    var Animal= {
+        Species: "Dog",  //Create KVPs for dictionary
+        Color: "White",
+        Breed: "Great Pyrenees",
+        Age: 7,
+        Sound: "Woof!"
+    };
+    document.getElementById("Dictionary1").innerHTML=Animal.Breed; //display the value of the chosen key
+}
+
+function my_Dictionary2() { //Define and Name function
+    var Animal= {
+        Species: "Cat",  //Create KVPs for dictionary
+        Color: "Orange",
+        Breed: "Tabby",
+        Age: 8,
+        Sound: "Meow!"
+    };
+    delete Animal.Breed; //Remove the key Breed from dictionary
+    document.getElementById("Dictionary2").innerHTML=Animal.Breed; //Attempt to display the value of the deleted key
+}
